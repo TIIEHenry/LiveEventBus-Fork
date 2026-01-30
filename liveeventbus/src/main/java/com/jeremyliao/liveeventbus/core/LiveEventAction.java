1a817ad5c97b56dd9ca84aeb1aab5327f2a034b7
罗洪来 <honglai.luo@dreamsmart.com>
2026-01-30 17:36:06 +0800

    新增id重复检查

--- liveeventbus/src/main/java/com/jeremyliao/liveeventbus/core/LiveEventAction.java
+++ liveeventbus/src/main/java/com/jeremyliao/liveeventbus/core/LiveEventAction.java
@@ -2,10 +2,17 @@ package com.jeremyliao.liveeventbus.core;
 
 import com.jeremyliao.liveeventbus.LiveEventBus;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+
 public class LiveEventAction<T> {
+    public static Set<String> allIds = new ConcurrentSkipListSet<>();
     public final String id;
 
     public LiveEventAction(String id) {
+        if (!allIds.add(id)) {
+            throw new IllegalStateException("duplicated id:" + id);
+        }
         this.id = id;
     }
 
