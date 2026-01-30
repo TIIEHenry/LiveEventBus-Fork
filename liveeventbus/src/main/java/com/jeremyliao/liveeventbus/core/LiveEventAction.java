b97e8802ff7658219e5f5e27ba36c523c98ef5f7
罗洪来 <honglai.luo@dreamsmart.com>
2026-01-30 17:36:06 +0800

    LiveEventAction直接实现Observable

--- liveeventbus/src/main/java/com/jeremyliao/liveeventbus/core/LiveEventAction.java
+++ liveeventbus/src/main/java/com/jeremyliao/liveeventbus/core/LiveEventAction.java
@@ -1,11 +1,15 @@
 package com.jeremyliao.liveeventbus.core;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.Observer;
+
 import com.jeremyliao.liveeventbus.LiveEventBus;
 
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
 
-public class LiveEventAction<T> {
+public class LiveEventAction<T> implements Observable<T> {
     public static Set<String> allIds = new ConcurrentSkipListSet<>();
     public final String id;
 
@@ -22,5 +26,70 @@ public class LiveEventAction<T> {
 
     public Observable<T> get() {
         return LiveEventBus.get(id);
+    }
+
+    @Override
+    public void post(T value) {
+        get().post(value);
+    }
+
+    @Override
+    public void postAcrossProcess(T value) {
+        get().postAcrossProcess(value);
+    }
+
+    @Override
+    public void postAcrossApp(T value) {
+        get().postAcrossApp(value);
+    }
+
+    @Override
+    public void postDelay(T value, long delay) {
+        get().postDelay(value, delay);
+    }
+
+    @Override
+    public void postDelay(LifecycleOwner sender, T value, long delay) {
+        get().postDelay(sender, value, delay);
+    }
+
+    @Override
+    public void postOrderly(T value) {
+        get().postOrderly(value);
+    }
+
+    @Override
+    public void broadcast(T value) {
+        get().broadcast(value);
+    }
+
+    @Override
+    public void broadcast(T value, boolean foreground, boolean onlyInApp) {
+        get().broadcast(value, foreground, onlyInApp);
+    }
+
+    @Override
+    public void observe(@NonNull LifecycleOwner owner, @NonNull Observer<T> observer) {
+        get().observe(owner, observer);
+    }
+
+    @Override
+    public void observeSticky(@NonNull LifecycleOwner owner, @NonNull Observer<T> observer) {
+        get().observeSticky(owner, observer);
+    }
+
+    @Override
+    public void observeForever(@NonNull Observer<T> observer) {
+        get().observeForever(observer);
+    }
+
+    @Override
+    public void observeStickyForever(@NonNull Observer<T> observer) {
+        get().observeStickyForever(observer);
+    }
+
+    @Override
+    public void removeObserver(@NonNull Observer<T> observer) {
+        get().removeObserver(observer);
     }
 }
